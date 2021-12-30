be042197faf9d705349cab699154a32f7a3654ba
Amod Kunwar <amodkunwar1997@gmail.com>
2021-12-30 13:26:27 +0530

    Reset password Ui is done

--- src/app/rest-password/rest-password.component.ts
+++ src/app/rest-password/rest-password.component.ts
@@ -1,4 +1,8 @@
+import { Router } from '@angular/router';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { Component, OnInit } from '@angular/core';
+import { ServiceService } from './../service/service.service';
+import { RegisterComponent } from './../register/register.component';
 
 @Component({
   selector: 'app-rest-password',
@@ -7,9 +11,41 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RestPasswordComponent implements OnInit {
 
-  constructor() { }
+  resetPassword: FormGroup;
+
+  constructor(private formBuilder: FormBuilder, private router: Router, private service: ServiceService) { 
+    this.resetPassword = this.formBuilder.group({
+      password1: ['',Validators.required],
+      password2: ['',Validators.required]
+    },{
+      validator: this.ConfirmedValidator('password1','password2')
+    });
+  }
 
   ngOnInit(): void {
+  }
+
+  changePassword() {
+
+  }
+
+  get fval() {
+    return this.resetPassword.controls;
+  }
+
+  ConfirmedValidator(controlName: string, matchingControlName: string){
+    return (formGroup: FormGroup) => {
+      const control = formGroup.controls[controlName];
+      const matchingContol = formGroup.controls[matchingControlName];
+      if(matchingContol.errors && !matchingContol.errors.ConfirmedValidator){
+        return;
+      }
+      if(control.value!==matchingContol.value){
+        matchingContol.setErrors({ConfirmedValidator: true});
+      }else{
+        matchingContol.setErrors(null);
+      }
+    }
   }
 
 }
